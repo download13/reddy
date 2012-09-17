800027ad567b02c9dbdb840cbf3b7efdc82b39b5
Erin <download333@gmail.com>
2012-09-17 01:10:20 -0700

    Works now

--- reddy.js
+++ reddy.js
@@ -1,15 +1,13 @@
 (function(context) {
-	context.ready = function(cb) {
-		var done = false;
-		
-		if(!check()) document.onreadystatechange = check;
-		
-		function check() {
-			if(document.readyState[0] != 'l') {
-				done || cb();
-				return done = true;
-			}
-			return false;
+	var cbs = [];
+	document.onreadystatechange = function() {
+		while(cbs.length > 0) {
+			cbs.unshift()();
 		}
+	}
+	
+	context.ready = function(cb) {
+		if(document.readyState[0] != 'l') cb();
+		else cbs.push(cb);
 	}
 })(this);
